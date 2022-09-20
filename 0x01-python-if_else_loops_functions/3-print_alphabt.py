de362ad7b7bbba581a3a22f3fe4ddb4ceeb7fc82
arsemakidist <kidist.shiferaw@aau.edu.et>
2022-09-20 10:05:48 -0700

    python 2nd task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')+1):
-if i == ord('e') or i == ord('q'):
-     continue
-     print('{:c}'.format(i), end="")
+for x in range(ord("a"), ord("z") + 1):
+if chr(x) != 'q' and chr(x) != 'e':
+    print("{}".format(chr(x)), end="")
