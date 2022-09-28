483230ebacd4387817107f41a66ad061a9de7991
arsemakidist <kidist.shiferaw@aau.edu.et>
2022-09-28 15:45:01 -0700

    my task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3
-# 12-roman_to_int.py
-# Amos Mwongela Gabriel <amosmwongelah@gmail.com>
-def roman_to_int(roman_string):
-    """Converts a roman numeral to an integer."""
-    if (not isinstance(roman_string, str) or
-            roman_string is None):
-        return (0)
-    roman_dict = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
-    }
-    num = 
-    for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
-            return (0)
-        if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
-         else:
-             num += roman_dict[roman_string[i]]
-    return (num)
+"""
+Roman to integer test file
+"""
+roman_to_int = __import__('12-roman_to_int').roman_to_int
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = 'IX'
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
