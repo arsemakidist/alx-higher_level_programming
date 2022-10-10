295088c6d449780b5fbf801b02fa3e54140d2279
arsemakidist <kidist.shiferaw@aau.edu.et>
2022-10-10 10:44:03 -0700

    my task

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    res = 0
-    for i in range(1, 3):
-        try:
-            if (i > a):
-                raise Exception("Too far")
-            else:
-                res += (a ** b) / i
-        except:
-            res = b + a
-            break
-    return res
+    result = 0
+    return result
