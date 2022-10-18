8e25ca57750aad97720b4dfa54aee2394a60a7d1
arsemakidist <kidist.shiferaw@aau.edu.et>
2022-10-18 12:31:52 -0700

    task 8

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,20 +2,26 @@
 # 1-rectangle.py
 # kidist shiferaw <kidist.shiferaw@aau.edu.et>
 """Defines a Rectangle class."""
+
+
 class Rectangle:
     """Represent a rectangle."""
+
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
+        
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
         self.width = width
         self.height = height
+   
     @property
     def width(self):
         """Get/set the width of the rectangle."""
         return self.__width
+    
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -23,10 +29,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+    
     @property
     def height(self):
         """Get/set the height of the rectangle."""
         return self.__height
+    
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
