9fc8ea751547852879e9b73c24122e6c39ad3b4c
arsemakidist <kidist.shiferaw@aau.edu.et>
2022-10-18 11:17:52 -0700

    my task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
-"""
-This is a Rectangle class.
-"""
-class Rectangle:
+"""Module for add_integer method."""
+def add_integer(a, b=98):
+    """Adds 2 integers.
+    Args:
+        a: the first int.
+        b: second int, default 98.
+    Raises:
+        TypeError: if a, b are neither int nor float.
+    Returns:
+        sum of the 2 integers.
     """
-    Create an empty Rectangle Object.
-    """
-    pass
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer") 
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
