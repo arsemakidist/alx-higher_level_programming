0572ea57b9d5f2658b7d416582b33fc68c323726
arsemakidist <kidist.shiferaw@aau.edu.et>
2022-10-24 11:41:55 -0700

    0x0A py task

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-""" add_attribute module """
+# 101-add_attribute.py
+# Kidist Shiferaw <kidist.shiferaw@aau.edu.et>
+"""Defines a function that adds attributes to objects."""
 
 
-def add_attribute(prmObject, prmName, prmValue):
-    """ add_attribute function """
-    if not hasattr(prmObject, "__dict__"):
+def add_attribute(obj, att, value):
+    """Add a new attribute to an object if possible.
+
+    Args:
+        obj (any): The object to add an attribute to.
+        att (str): The name of the attribute to add to obj.
+        value (any): The value of att.
+
+    Raises:
+        TypeError: If the attribute cannot be added.
+    """
+    if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
-    if (not hasattr(prmObject, prmName)):
-        prmObject.__setattr__(prmName, prmValue)
+    setattr(obj, att, value)
